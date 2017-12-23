cd67da778f7dacb1366c44f906b0522e951ced44
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-23 19:01:59 +0800

    Create remove_element.java

---
+++ remove_element.java
@@ -0,0 +1,10 @@
+package remove_elements;
+
+public class remove_elements {
+	public static void main(String[] args){
+		solution s=new solution();
+		int[] array={2,2,3,3,2};
+		s.removeElement(array, 3);
+	}
+
+}
